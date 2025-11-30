d868c82056ac3e542dffdf7ab3bedf43f6f56b6a
porfiriy <porf2003@mail.ru>
2025-11-30 14:40:38 +0300

    init repo

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import { Layout } from './components/Layout';
+import './styles/styles.scss';
+import './App.scss'
+
+function App() {
+
+  return (
+    <>
+      <Layout />
+
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+   darkMode: ["class", '[data-theme="dark"]'],
+   theme: {
+      extend: {
+         colors: {
+         },
+      },
+   },
+   plugins: [],
+};
